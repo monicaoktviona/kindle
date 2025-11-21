35d42ac93f1e69e2ff45f28e6674bb31233ef634
monicaoktviona <monicaoktaviona2002@gmail.com>
2025-11-21 09:39:48 +0700

    chore: config eslint

--- app/routes/upload-dictionary.tsx
+++ app/routes/upload-dictionary.tsx
@@ -1,10 +1,11 @@
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router";
+
+import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
-import { Link } from "react-router";
-import { readSqlite } from "~/utils/readSql";
 import { useVocabStore } from "~/store/vocabStore";
-import { Button } from "~/components/ui/button";
-import { useTranslation } from "react-i18next";
+import { readSqlite } from "~/utils/readSql";
 
 function UploadDictionary() {
   const { setRows } = useVocabStore();
